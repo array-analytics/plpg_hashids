5806579e082e5be557c9b7e1884466be3e999c28
Robert Hux <rhux@array-advisors.com>
2019-04-17 15:48:22 -0400

    Update create_hashids_schema.sql
    language fix

--- src/create_hashids_schema.sql
+++ src/create_hashids_schema.sql
@@ -1,3 +1,3 @@
 -- our usage of these functions places all of them in a separate schema, with "hashids" being the default name
--- change this or don't use it (but know that the other sql functions will not work out of the box, they are assume hashids schema). 
+-- change this or don't use it (but know that the other sql functions will not work out of the box, they are assuming the hashids schema). 
 CREATE SCHEMA hashids;
